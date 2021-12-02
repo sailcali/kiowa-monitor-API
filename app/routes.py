44bad37eae4291a79b9941056c7e6d9be577ebc4
Parsec <sailcali@gmail.com>
2021-12-01 17:35:06 -0800

    Added endpoint for today's temps

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,14 @@ from flask import Blueprint, jsonify, make_response, request, abort
 from flask.signals import request_finished
 from app import db
 from app.models import VenstarTemp
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 venstar_bp = Blueprint('venstar_bp', __name__, url_prefix='/temps')
 login_bp = Blueprint('login_bp', __name__, url_prefix='/')
 
 @login_bp.route("", methods=['GET'])
 def login():
-    return jsonify([])
+    return jsonify(['/temps', 'temps/<requested_date>', '/temps/today'])
 
 @venstar_bp.route("", methods=['GET'])
 def display_recent_temps():
@@ -35,4 +35,14 @@ def display_temps_by_date(requested_date):
     for temp in temps:
         data[datetime.strftime(temp.time, '%Y-%b-%d %H:%M')] = [temp.local_temp, temp.pi_temp, temp.remote_temp, temp.humidity]
     return make_response(data)
-    
+
+@venstar_bp.route("/today", methods=["GET"])
+def display_temps_from_today():
+    start_date = date.today()
+    start_time = datetime.combine(start_date, datetime.min.time()) 
+
+    temps = VenstarTemp.query.filter(VenstarTemp.time>start_time).all()
+    data = {}
+    for temp in temps:
+        data[datetime.strftime(temp.time, '%Y-%b-%d %H:%M')] = [temp.local_temp, temp.pi_temp, temp.remote_temp, temp.humidity]
+    return make_response(data)
