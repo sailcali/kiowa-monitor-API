410c19134d8a4a4e5cea459191cd6189b425450a
Parsec <sailcali@gmail.com>
2021-12-08 22:35:45 -0800

    Fixed the requested date endpoint

--- app/routes.py
+++ app/routes.py
@@ -95,20 +95,29 @@ def display_temps_by_date(requested_date):
     end_time = start_date + timedelta(days=1)
     temps = VenstarTemp.query.filter(VenstarTemp.time>start_date, VenstarTemp.time<end_time).all()
     data = {'data': []}
-    for i in range(len(temps)):
-        if i > 0:
-            last_heat_time = temps[i-1].heat_runtime
-            last_cool_time = temps[i-1].cool_runtime
-        else:
-            last_heat_time = 0
-            last_cool_time = 0
-        data['data'].append({'time': datetime.strftime(temps[i].time, '%Y-%b-%d %H:%M'),
-                     'local_temp': temps[i].local_temp,
-                     'pi_temp': temps[i].pi_temp,
-                     'remote_temp': temps[i].remote_temp,
-                     'humidity': temps[i].humidity,
-                     'heat_time': temps[i].heat_runtime - last_heat_time,
-                     'cool_time': temps[i].cool_runtime - last_cool_time})
+    if start_date < datetime.strptime('2021-12-05', '%Y-%m-%d'):
+        for i in range(len(temps)):
+        
+            data['data'].append({'time': datetime.strftime(temps[i].time, '%Y-%b-%d %H:%M'),
+                        'local_temp': temps[i].local_temp,
+                        'pi_temp': temps[i].pi_temp,
+                        'remote_temp': temps[i].remote_temp,
+                        'humidity': temps[i].humidity})
+    else:
+        for i in range(len(temps)):
+            if i > 0:
+                last_heat_time = temps[i-1].heat_runtime
+                last_cool_time = temps[i-1].cool_runtime
+            else:
+                last_heat_time = 0
+                last_cool_time = 0
+            data['data'].append({'time': datetime.strftime(temps[i].time, '%Y-%b-%d %H:%M'),
+                        'local_temp': temps[i].local_temp,
+                        'pi_temp': temps[i].pi_temp,
+                        'remote_temp': temps[i].remote_temp,
+                        'humidity': temps[i].humidity,
+                        'heat_time': temps[i].heat_runtime - last_heat_time,
+                        'cool_time': temps[i].cool_runtime - last_cool_time})
     return render_template('index.html', data=data)
 
 @temps_bp.route("/today_api", methods=["GET"])
