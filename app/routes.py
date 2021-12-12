0587eb11aaafd6b21b77b7f879c87f25d20a8030
Parsec <sailcali@gmail.com>
2021-12-11 17:09:58 -0800

    made the blank endpoint reach dashboard for now

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,7 @@ login_bp = Blueprint('login_bp', __name__, url_prefix='/')
 
 @login_bp.route("", methods=['GET'])
 def login():
-    return jsonify(['/temps', 'temps/<requested_date>', '/temps/today', '/temps/today_api'])
+    return redirect(url_for('venstar_bp.venstar_dashboard'))
 
 @venstar_bp.route("", methods=['GET'])
 def venstar_dashboard():
@@ -66,20 +66,6 @@ def venstar_changes():
     }
     requests.post(VENSTAR_CONTROL_URL, params=params)
     return redirect(url_for('venstar_bp.venstar_dashboard'))
-    # password = request.form.get('password')
-    # remember = True if request.form.get('remember') else False
-
-    # user = User.query.filter_by(email=email).first()
-
-    # # check if the user actually exists
-    # # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    # if not user or not check_password_hash(user.password, password):
-    #     flash('Please check your login details and try again.')
-    #     return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
-
-    # # if the above check passes, then we know the user has the right credentials
-    # login_user(user, remember=remember)
-    # return redirect(url_for('main.profile'))
 
 @temps_bp.route("", methods=['GET'])
 def display_recent_temps():
