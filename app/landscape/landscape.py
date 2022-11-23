ea9aaed995e8d5437f2c8cf75c0c11fd427f71dd
Parsec <sailcali@gmail.com>
2022-11-22 21:18:22 -0800

    landscape lighting wont be locked on after sunset

--- app/landscape/landscape.py
+++ app/landscape/landscape.py
@@ -2,14 +2,12 @@
 
 from astral.sun import sun
 from datetime import date
-import os
 import pytz
 from astral.geocoder import database, lookup
 from datetime import datetime, timedelta
 import requests
-# import configparser
 
-DIRECTORY = os.environ.get("DIRECTORY")
+CRON_PERIOD = 6
 
 def change_landscape(on_off=3, delay_request=False):
     """Algorithm for deciding state of landscape lighting.
@@ -19,19 +17,6 @@ def change_landscape(on_off=3, delay_request=False):
     DEP: delay_request = {datetime} : gets logged in config file and used as delay param
     DEP: delay_request = false : config file is used as delay param"""
     
-    # Setup logger and config file
-    # config = configparser.ConfigParser()
-    # config.read_file(open(f'{DIRECTORY}/delay_time.conf'))
-    
-    # Set the config file to the new delay request if given
-    # Then set the current delay datetime
-    # if delay_request:
-    #     config.set('DelayDatetime', 'value', datetime.strftime(delay_request, '%Y-%m-%d %H:%M:%S'))
-    #     with open(f'{DIRECTORY}/delay_time.conf', 'w') as configfile:
-    #         config.write(configfile)
-    # value = config.get('DelayDatetime', 'value')
-    # delay_datetime = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
-    
     # This will track if the lights are actually changed or not
     state_change = None
 
@@ -39,7 +24,9 @@ def change_landscape(on_off=3, delay_request=False):
     city = lookup("San Diego", database())
     s = sun(city.observer, date=date.today())
     sunset = s["sunset"] + timedelta(minutes=15)
+    sunset_over = sunset + timedelta(minutes=CRON_PERIOD)
     sunset = sunset.astimezone(tz=pytz.timezone("US/Pacific"))
+    sunset_over = sunset_over.astimezone(tz=pytz.timezone("US/Pacific"))
 
     # Get the current settings from PICO
     response = requests.get('http://192.168.86.33/get-status')
@@ -59,12 +46,15 @@ def change_landscape(on_off=3, delay_request=False):
                 state_change = True
 
         # If sunset has occurred and lights are off, turn it on
-        if current_status == 0 and sunset.time() < datetime.now().time():
+        if current_status == 0 and \
+            sunset_over.time() > datetime.now().time() and \
+                sunset.time() < datetime.now().time():
             response = requests.get('http://192.168.86.33/lights/on')
             if response.json()['current_status']['landscape'] == 1:
                 state_change = True
+        
         # If its midnight turn the landscape lights off
-        if datetime.now().hour == 0 and current_status == 1:
+        if current_status == 1 and datetime.now().hour == 0:
             response = requests.get('http://192.168.86.33/lights/off')
             if response.json()['current_status']['landscape'] == 0:
                 state_change = False
